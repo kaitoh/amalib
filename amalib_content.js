79ed45d92961b9e74183a8a52e32eb18ed821d47
ksaitoh <ksaitoh560@gmail.com>
2014-12-18 15:26:04 +0900

    Fix to get prouct title

--- amalib_content.js
+++ amalib_content.js
@@ -2,5 +2,5 @@
 chrome.extension.sendRequest(
         {asin: document.getElementById("ASIN").value, 
             location: location, 
-            title: document.getElementById("btAsinTitle").textContent }, 
+            title: document.getElementById("productTitle").textContent }, 
             function(response) { });
